d303feb434dc488deacb743f2a5b2571cdc771b9
Stella Laurenzo <stella@laurenzo.org>
2011-06-28 19:51:05 -0700

    Refactor makefiles to use mapnik-config and add junit to build

--- csrc/common.mk
+++ csrc/common.mk
@@ -1,1 +1,6 @@
+JAVA_CMD ?= java
+CFLAGS ?= -O3
+JAVA_CFLAGS ?= $(shell $(JAVA_CMD) -jar java-config.jar --cflags)
+MAPNIK_CFLAGS = $(shell mapnik-config --cflags)
+MAPNIK_LIBS = $(shell mapnik-config --libs) $(shell pkg-config --cflags cairomm-1.0)
 
